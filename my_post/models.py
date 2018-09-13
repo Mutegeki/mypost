5c8dfdbb30ffbdd418e5b81eaa0b0078c2787c5d
Mutegeki <braynmute@gmail.com>
2018-09-13 04:07:20 -0400

    Changed the name to first and added the last

--- my_post/models.py
+++ my_post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.forms import ModelForm
 # Create your models here.
+
+
+class Post(models.Model):
+	first = models.CharField(max_length=25, blank=True)
+    last = models.CharField(max_length=25,blank=True)
+	phone_number = models.CharField(max_length=17, blank=True)
+	email = models.EmailField(max_length=70, blank=True, null=True, unique=True)
+	social = models.CharField(max_lengt=50)
