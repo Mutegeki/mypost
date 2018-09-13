58ded5f9a752894fba11630bc4fd76c93583c6ce
Mutegeki <braynmute@gmail.com>
2018-09-13 05:05:36 -0400

    Extended the templates

--- my_post/models.py
+++ my_post/models.py
@@ -9,3 +9,6 @@ class Post(models.Model):
 	phone_number = models.CharField(max_length=17, blank=True)
 	email = models.EmailField(max_length=70, blank=True, null=True, unique=True)
 	social = models.CharField(max_lengt=50)
+
+    def __str__(self):
+		return self.name
